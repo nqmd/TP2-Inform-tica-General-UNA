c65ca65106c64fc7f2562622bd9dbc3c96cece0a
warMachine <warmachine@pop-os.localdomain>
2021-09-19 19:48:26 -0300

    Se genera primer prototipo de menu principal, es necesario desarrollo

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,5 @@
+//Joshua Frías
+
 #include <iostream>
 #include <ctime>
 #include <string>
@@ -8,34 +10,80 @@ using namespace std;
 
 // Variables globales
 bool finJuego;
-int ancho = 120;
-int alto = 40;
+int yMax, xMax;
 
 // Declaración de Funciones
+void intro();
 void pantallaInicio();
 
 int main()
 {
 
 	initscr();
-	pantallaInicio();	
+	noecho();
+	curs_set(0);
+	getmaxyx(stdscr, yMax, xMax);
+
+	pantallaInicio();
+
 	endwin();
 	return 0;
 }
 
 void pantallaInicio()
 {
-	WINDOW * win = newwin(40,120,0,0);
-	refresh();
-	box(win, 0, 0);
-	mvwprintw(win, 10, 35, "Llora una vez, pero no llores dos veces;");
-	mvwprintw(win, 11, 35, "tus primeras lágrimas me regalan un río que cruzar,");
-	mvwprintw(win, 12, 35, "pero las siguientes provocan tormentas imposibles de salvar...");
-	mvwprintw(win, 13, 35, "Llora una vez, pero no llores dos;");
-	mvwprintw(win, 14, 35, "te prometo ser feliz allá donde voy.");
-	mvwprintw(win, 16, 80, "-Crónicas del Mictlán");
-	wrefresh(win);
-	
-	refresh();
-	getch();
+
+	WINDOW *wmenu = newwin(yMax / 2, xMax / 2, yMax / 4, xMax / 4);
+
+	box(wmenu, 0, 0);
+
+	//Firmas
+	wattron(wmenu, A_DIM);
+	mvwprintw(wmenu, 0, 5, "IG II");
+	mvwprintw(wmenu, 19, 30, "Made by Icarus Black Magic");
+
+	//Opciones
+	wattron(wmenu, A_BLINK);
+	wattron(wmenu, A_BOLD);
+	mvwprintw(wmenu, 7, 26, "JUGAR");
+	mvwprintw(wmenu, 9, 22, "INSTRUCCIONES");
+	mvwprintw(wmenu, 11, 26, "SALIR");
+
+	char letra;
+
+	while (letra = wgetch(wmenu))
+	{
+		switch (letra)
+		{
+		case 'j':
+			wattron(wmenu, A_STANDOUT);
+			mvwprintw(wmenu, 7, 26, "JUGAR");
+			wattroff(wmenu, A_STANDOUT);
+			mvwprintw(wmenu, 9, 22, "INSTRUCCIONES");
+			mvwprintw(wmenu, 11, 26, "SALIR");
+			break;
+
+		case 'i':
+			wattron(wmenu, A_STANDOUT);
+			mvwprintw(wmenu, 9, 22, "INSTRUCCIONES");
+			wattroff(wmenu, A_STANDOUT);
+			mvwprintw(wmenu, 7, 26, "JUGAR");
+			mvwprintw(wmenu, 11, 26, "SALIR");
+			break;
+
+		case 's':
+			werase(wmenu);
+			endwin();
+			exit(EXIT_SUCCESS);
+			break;
+
+		default:
+			wattron(wmenu, A_BLINK);
+			wattron(wmenu, A_BOLD);
+			mvwprintw(wmenu, 7, 26, "JUGAR");
+			mvwprintw(wmenu, 9, 22, "INSTRUCCIONES");
+			mvwprintw(wmenu, 11, 26, "SALIR");
+			break;
+		}
+	}
 }
